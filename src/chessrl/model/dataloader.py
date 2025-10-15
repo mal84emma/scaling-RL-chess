6f348d12717168b7211a8f3f20eda74819758959
mal84emma <89249742+mal84emma@users.noreply.github.com>
2025-10-15 11:19:07 +0100

    (Closes #14) Update data loader to process position dataset

--- src/chessrl/model/dataloader.py
+++ src/chessrl/model/dataloader.py
@@ -1,65 +1,39 @@
-__all__ = ("GameDataSequence",)
+__all__ = ("PositionDataSequence",)
 
 import numpy as np
-from keras.utils import Sequence, to_categorical
+from keras.utils import Sequence
 
-import chessrl
-from chessrl.dataset import GameDataset
+from chessrl.dataset import PositionDataset
 
 from .encoder import get_game_state
 
-# need to figure out how to handle both training data generated from simulated
-# stockfish games, and training data that is generated at test-time which only
-# has position-score information.
-# this probably means I want to do a pre-processing of the training games,
-# maybe in the training script that preps them into a data loader, playing out
-# the games, scoring positions, and loading the position-score pairs into the
-# data loader.
 
-
-class GameDataSequence(Sequence):
-    """Transforms a Dataset to a Data generator to be fed to the training
+class PositionDataSequence(Sequence):
+    """Transforms a dataset to a data generator to be fed to the training
     loop of the neural network.
 
     Attributes:
-        dataset: GameDataset. Dataset of fames
-        batch_size: int. Nb of board representations of each batch
-        uci_ids: dict. Encoding the move UCI labels to one-hot.
-        random_flips: float. Proportion of board representation which will
-                        be flipped 180 degrees.
+        dataset: PositionDataset. Dataset of game_state-score pairs.
+        batch_size: int. # of board representations of each batch.
     """
 
-    def __init__(self, dataset: GameDataset, batch_size: int = 8, random_flips=0):
+    def __init__(
+        self,
+        dataset: PositionDataset,
+        batch_size: int = 10,
+    ):
         self.dataset = dataset
         self.batch_size = min(batch_size, len(dataset))
-        self.uci_ids = {u: i for i, u in enumerate(chessrl.UCI_MOVES)}
-        self.random_flips = random_flips
 
     def __len__(self):
         return int(len(self.dataset) / self.batch_size)
 
     def __getitem__(self, idx):
-        batch = self.dataset[idx * self.batch_size : (idx + 1) * self.batch_size]
-        batch_x = []  # Board reprs
-        batch_y_policies = []
-        batch_y_values = []
+        game_states, scores = self.dataset[
+            idx * self.batch_size : (idx + 1) * self.batch_size
+        ]
 
-        for i in batch:
-            i_augmented = self.dataset.augment_game(i)
+        batch_x = [get_game_state(state) for state in game_states]  # board reprs
+        batch_y = scores  # target scores
 
-            flip = np.random.rand() < self.random_flips
-            batch_x.extend(
-                [get_game_state(i_g["game"], flipped=flip) for i_g in i_augmented]
-            )
-            batch_y_policies.extend(
-                [
-                    to_categorical(self.uci_ids[targets["next_move"]], num_classes=1968)
-                    for targets in i_augmented
-                ]
-            )
-            batch_y_values.extend([targets["result"] for targets in i_augmented])
-
-        return np.asarray(batch_x), (
-            np.asarray(batch_y_policies),
-            np.asarray(batch_y_values),
-        )
+        return np.asarray(batch_x), np.asarray(batch_y)
